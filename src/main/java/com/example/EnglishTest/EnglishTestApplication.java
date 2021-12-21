0190a1361ad9e71ac431c09af200e9f9eb6a46c0
Mikhail Meleshkin <mikhail.meleshkin@gmail.com>
2021-12-21 18:09:35 +0300

    Init commit

---
+++ src/main/java/com/example/EnglishTest/EnglishTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.EnglishTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EnglishTest/EnglishTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EnglishTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
