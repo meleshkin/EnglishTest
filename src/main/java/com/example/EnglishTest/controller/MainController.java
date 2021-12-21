8881d66902e490f44c185a2ad9ba2e35f7961f6f
Mikhail Meleshkin <mikhail.meleshkin@gmail.com>
2021-12-21 20:17:43 +0300

    Added test entity and repo

--- src/main/java/com/example/EnglishTest/controller/MainController.java
+++ src/main/java/com/example/EnglishTest/controller/MainController.java
@@ -1,14 +1,26 @@
 package com.example.EnglishTest.controller;
 
+import com.example.EnglishTest.model.Test;
+import com.example.EnglishTest.service.TestRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class MainController {
 
+    private final TestRepository testRepository;
+
+    public MainController(TestRepository testRepository) {
+        this.testRepository = testRepository;
+    }
+
     @GetMapping("/")
     @SuppressWarnings("unused")
     public String handleMain() {
+        List<Test> allTests = testRepository.findAll();
+        allTests.forEach(test -> System.out.println(test.getName()));
         return "main.html";
     }
 }
---
+++ src/main/java/com/example/EnglishTest/model/Test.java
@@ -0,0 +1,37 @@
+package com.example.EnglishTest.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Test {
+    @Id
+    @SequenceGenerator(name="id_seq", sequenceName="id_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="id_seq")
+    private long id;
+    private String name;
+    private String description;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
